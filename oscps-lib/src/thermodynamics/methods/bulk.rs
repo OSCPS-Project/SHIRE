0145054d181fa14a2b3eb5f49981f7412b5583cc
Bhargav Akula <mail.bhargavakula@gmail.com>
2026-04-19 19:07:01 -0400

    working on methods that will calculate the different properties using the EOS models
    currently setting up the file structure

---
+++ oscps-lib/src/thermodynamics/methods/bulk.rs
@@ -0,0 +1,6 @@
+//! #BulkPropertySolver
+//!
+//! Main file for bulk property calculations
+//!
+//! Will go by first calculating the unknown state variables (given the variables we do
+//! know/provided) and then use that to solve for the bulk properties
--- oscps-lib/src/thermodynamics/methods/single_component.rs
+++ oscps-lib/src/thermodynamics/methods/single_component.rs
@@ -1,6 +1,12 @@
-//! SingleComponent
+//! #SingleComponentPropertySolver
 //!
-//! For determining the single component properties
+//! Main lib file for single component property calculations
+//! 
+//! This will include VLE (single component), critical points, triple point, 
+//! melting, sublimation, widom
 
 
 use crate::thermodynamics::methods;
+
+
+
