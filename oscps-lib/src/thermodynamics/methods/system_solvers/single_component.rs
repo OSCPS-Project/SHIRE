bd854b91b5c4ff908a35e9be1740674761f57022
Bhargav Akula <mail.bhargavakula@gmail.com>
2026-05-31 19:14:08 -0400

    setting up methods

--- oscps-lib/src/thermodynamics/methods/system_solvers/single_component.rs
+++ oscps-lib/src/thermodynamics/methods/system_solvers/single_component.rs
@@ -1,22 +1,58 @@
 //! #SingleComponent
 //!
 //! Contains lib file for property solvers for a single component system
+
+// internal imports
 use crate::thermodynamics::ideal::BaseEOSModel;
+
+// modules that are part of the single component system solver
+pub mod saturation;
+
+// external imports
 use uom::si::f64::*;
 
 /// #SingleComponentSystemSolver
 pub struct SingleComponentSystemSolver {
     /// Equation of state model
     pub model: Option<Box<dyn BaseEOSModel>>,
-    /// volume
-    pub volume: Volume,
+    /// liquid phase volume
+    pub v_L: Volume,
+    /// vapor phase volume
+    pub v_V: Volume,
     /// Temperature
     pub temperature: ThermodynamicTemperature,
     /// moles
     pub moles: AmountOfSubstance
-
 }
 
-pub trait SingleComponentBaseMethods {
+impl SingleComponentSystemSolver {
+    /// checks if values are positive
+    fn _check_positive_values() {
 
+    }
+    
+    ///thermodynamic derivative routine that returns how molar volume changes 
+    ///with pressure at fixed temperature (and composition). Used in the validity
+    ///checks function
+    fn _dv_dp_calculation() {
+
+    }
+
+    /// validity checks for mechanical stability of volumes and that liquid and vapor phase
+    /// volumes are different by a threshold
+    pub fn sc_validity_checks() {
+        
+    }
+
+    /// base function equilibrium pressure calculation
+    pub fn sc_compute_equilibrium_pressure() {
+
+    }
+
+    /// base function equilibrium temperature calculation
+    pub fn sc_compute_equilibrium_temperature() {
+
+    }
 }
+
+
---
+++ oscps-lib/src/thermodynamics/methods/system_solvers/single_component/saturation.rs
@@ -0,0 +1,17 @@
+//! #SingleComponentSaturationMethods
+
+
+
+/// Will hold the methods for computing saturation pressure and temperature
+pub trait SCAntoineSat {
+
+    pub fn psat_computation() {
+
+    }
+
+    pub fn tsat_computation() {
+
+    }
+}
+
+
